48c00d9267f67b6631bf8a055f465fcd076a3258
Joelomojefe <joelomojefe@gmail.com>
2025-05-27 09:20:59 +0100

    Fix script path in ecosystem config to point to the correct JavaScript file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'liveness-mogrex-api',
-      script: 'dist/index.ts',
+      script: 'dist/index.js',
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
